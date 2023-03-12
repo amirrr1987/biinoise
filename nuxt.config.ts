e7a3f4e95c2eba448597b9f1f78c0f59d84edbb7
Amir Maghami <Maghami.A1987@gmail.com>
2023-03-13 01:38:11 +0330

    feat: install nuxt and theme

---
+++ nuxt.config.ts
@@ -0,0 +1,44 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: [
+    // "https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800&amp;display=swap",
+    "~/assets/css/vendor/bootstrap.min.css",
+    "~/assets/css/vendor/ionicons.min.css",
+    "~/assets/css/vendor/simple-line-icons.css",
+    "~/assets/css/vendor/font-awesome.min.css",
+    "~/assets/css/plugins/animate.css",
+    "~/assets/css/plugins/jquery-ui.min.css",
+    "~/assets/css/plugins/swiper.css",
+    "~/assets/css/vendor/vendor.min.css",
+    "~/assets/css/plugins/plugins.min.css",
+    "~/assets/css/style.min.css",
+    "~/assets/css/style.css",
+  ],
+  plugins: [
+    // "~/assets/js/vendor/vendor.min.js",
+    // "~/assets/js/plugins/plugins.min.js",
+    // "~/assets/js/main.js",
+  ],
+
+  // { src: '~/plugins/server-only.js', body: true }
+
+  // scripts: [
+  //   { src: "/js/jquery-1.12.3.min.js", body: true },
+  //   { src: "/js/jquery.easing.min.js", body: true },
+  //   { src: "/js/jquery.waypoints.min.js", body: true },
+  //   { src: "/js/jquery.counterup.min.js", body: true },
+  //   { src: "/js/popper.min.js", body: true },
+  //   { src: "/js/bootstrap.min.js", body: true },
+  //   { src: "/js/isotope.pkgd.min.js", body: true },
+  //   { src: "/js/infinite-scroll.min.js", body: true },
+  //   { src: "/js/imagesloaded.pkgd.min.js", body: true },
+  //   { src: "/js/slick.min.js", body: true },
+  //   { src: "/js/contact.js", body: true },
+  //   { src: "/js/validator.js", body: true },
+  //   { src: "/js/wow.min.js", body: true },
+  //   { src: "/js/morphext.min.js", body: true },
+  //   { src: "/js/parallax.min.js", body: true },
+  //   { src: "/js/jquery.magnific-popup.min.js", body: true },
+  //   { src: "/js/custom.js", body: true },
+  // ],
+});
